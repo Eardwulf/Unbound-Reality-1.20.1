b82a89cfb6622d7a685f65901fddb5325df2604d
Eardwulf <egs1990@outlook.com>
2023-11-28 23:07:16 -0600

    Initial commit

---
+++ src/main/java/unboundreality/ubrcore/UbrCore.java
@@ -0,0 +1,11 @@
+package unboundreality.ubrcore;
+
+import net.minecraftforge.fml.common.Mod;
+
+
+
+@Mod(UbrCore.MODID)
+public class UbrCore {
+    public static final String MODID = "ubrcore";
+
+}
